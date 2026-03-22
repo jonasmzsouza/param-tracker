4aa5a1516357e89fce0cd0fdcafaabf06d64fdab
jonasmzsouza <61324433+jonasmzsouza@users.noreply.github.com>
2026-03-22 19:23:25 +0000

    :bricks: chore(release): prepare version 4.0.1 [skip ci]

--- dist/tracker.cjs.js
+++ dist/tracker.cjs.js
@@ -1,4 +1,4 @@
-/*! ParamTracker 4.0.0 | MIT License | (c) Jonas Souza 2023-2026 | https://github.com/jonasmzsouza/param-tracker */
+/*! ParamTracker 4.0.1 | MIT License | (c) Jonas Souza 2023-2026 | https://github.com/jonasmzsouza/param-tracker */
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
